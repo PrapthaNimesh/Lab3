ee504d87ee75df2ea43418c6642888ba75b1f0d9
PrapthaNimesh <it18217032@my.sliit.lk>
2020-03-26 19:19:43 +0530

    Lab sheet 3

---
+++ app/src/main/java/com/example/labsheet3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.labsheet3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity<staic> extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public static final String
+            EXTRA_MESSAGE="project";
+    public void sendMessage(View view){
+
+
+        Intent intent=new Intent(this, DisplayMessageActivity.class);
+        EditText editText=(EditText) findViewById(R.id.editText);
+        String message=editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
+}
